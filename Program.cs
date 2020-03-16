f181f40991470d7281f6d7cffa0c8d2cc2350ab6
Jack-Andersen <Jack-Andersen@users.noreply.github.com>
2020-03-16 11:25:07 +0100

    Adding new file

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Edabit__the_2_weeks_we_re_not_in_school
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Hurdle Jump!
+            
+            Create a function that takes an array of hurdle heights and a jumper's jump height, and
+            determine whether or not the hurdler can clear all the hurdles.
+
+            A hurdler can clear a hurdle if their jump height is greater than equal to the hurdle height.
+
+            hurdleJump([1, 2, 3, 4, 5], 5) ➞ true
+
+            hurdleJump([5, 5, 3, 4, 5], 3) ➞ false
+
+            hurdleJump([5, 4, 5, 6], 10) ➞ true
+
+            hurdleJump([1, 2, 1], 1) ➞ false
+
+            Notes
+
+            Return true for the edge case of an empty array of hurdles. (Zero hurdles means that any jump height can clear them).
+
+            */
+
+            //int[] hurdles = { 1, 2, 3, 4, 5 };
+
+            //int jumpHeight = 10;
+
+            //foreach (int num in hurdles)
+            //{
+
+            //    if (num > jumpHeight)
+
+            //    {
+            //        Console.WriteLine("True");
+            //    }
+
+            //}
+            //  Console.WriteLine("true");
+
+            //Console.ReadKey();
+
+
+            /*Convert Number to Corresponding Month Name!
+             
+            Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string.
+            For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
+
+            You can expect only integers ranging from 1 to 12 as test input.
+            
+            */
+
+            //int num = 1;
+
+            //for (int i = 0; i < num; num++)
+            //{
+
+            //    DateTime date = new DateTime(2010, num, 1);
+            //    Console.WriteLine(date.ToString("MMMM"));
+
+            //}
+
+            //Console.ReadKey();
+
+            /* Count Instances of a Character in a String!
+             
+             Create a function that takes a character and a string as arguments and returns the number of times the character is found in the string.
+             
+             CharCount("a", "edabit") ➞ 1
+
+             CharCount("c", "Chamber of secrets") ➞ 1
+
+             CharCount("b", "big fat bubble") ➞ 4
+
+             Your output must be case-sensitive (see second example).
+
+             */
+
+
+
+        }
+    }
+}
