0f6cef6dc26a1785cc74d30259b6769b243c1423
Jack-Andersen <Jack-Andersen@users.noreply.github.com>
2020-03-17 12:36:43 +0100

    Done with 4, working on,5,6,7

--- Program.cs
+++ Program.cs
@@ -107,7 +107,86 @@ namespace Edabit__the_2_weeks_we_re_not_in_school
 
 
 
-            /* Absolute Sum! <--- 4th
+            /* Hamming Distance! <--- 4th
+
+            Hamming distance is the number of characters that differ between two strings.
+
+            String1: "abcbba"
+            String2: "abcbda"
+
+            Hamming Distance: 1 - "b" vs. "d" is the only difference.
+
+            Create a function that computes the hamming distance between two strings.
+
+            HammingDistance("abcde", "bcdef") ➞ 5
+
+            HammingDistance("abcde", "abcde") ➞ 0
+
+            HammingDistance("strong", "strung") ➞ 1
+
+            Both strings will have the same length.
+
+            */
+
+            //string str1 = "Katte";
+            //string str2 = "Kattt";
+
+            //int i = 0, count = 0;
+            //while (i < str1.Length)
+            //{
+            //    if (str1[i] != str2[i])
+            //        count++;
+            //    i++;
+            //}
+            //Console.WriteLine(count);
+
+            //Console.ReadKey();
+
+
+
+            /* Smaller String Number <--- 5th
+
+            Create a function that returns the smaller number.
+
+            smallerNum("21", "44") ➞ "21"
+
+            smallerNum("1500", "1") ➞ "1"
+
+            smallerNum("5", "5") ➞ "5"
+
+            Numbers will be represented as strings, and your output should also be a string.
+            If both numbers tie, return either number.
+            Numbers will be positive.
+            Bonus: See if you can do this without converting to integers.
+
+            */
+
+
+
+            Console.ReadKey();
+
+
+
+            /* To the Power of____! <--- 6th
+             
+            Create a function that takes a base number and an exponent number and returns the calculation.
+
+            СalculateExponent(5, 5) ➞ 3125
+
+            СalculateExponent(10, 10) ➞ 10000000000
+
+            СalculateExponent(3, 3) ➞ 27
+
+            All test inputs will be positive integers.
+            Don't forget to return the result.
+
+            */
+
+
+
+
+
+            /* Absolute Sum! <--- 7th
               
             Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
 
@@ -123,8 +202,14 @@ namespace Edabit__the_2_weeks_we_re_not_in_school
 
             */
 
+            //int[] arr = {2, 0, -1, 10, -2};
 
+            //foreach (int value in arr)
+            //{
+            //    Console.WriteLine(Math.Abs(value));
+            //}
 
+            //Console.ReadKey();??
 
         }
     }
