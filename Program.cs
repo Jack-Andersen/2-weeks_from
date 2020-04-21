b8e76aaae7f95ecd62a7d1bcb5f2b254db00f8b3
Jack-Andersen <Jack-Andersen@users.noreply.github.com>
2020-04-21 14:45:42 +0200

    Done with 39,40. Working on 41.

--- Program.cs
+++ Program.cs
@@ -1266,6 +1266,70 @@ S           ubReddit("https://www.reddit.com/r/mildlyinteresting/") ➞ "mildlyi
 
             */
 
+            //string str = "Happy";
+
+            //var charArray = str.ToLower().ToCharArray();
+            //Array.Sort(charArray);
+
+            //for (int i = 0; i < str.Length - 1; i++)
+            //{
+            //    if (charArray[i] == charArray[i + 1])
+            //        Console.WriteLine(false);
+            //}
+
+            //Console.WriteLine(true);
+
+            //Console.ReadKey();
+
+
+
+            /* Find the Characters Counterpart Char Code! <--- 40th
+            
+            Create a function that takes a single character as an argument and returns the char code of its lowercased / uppercased counterpart.
+
+            Given that:
+              - "A" char code is: 65
+              - "a" char code is: 97
+
+            CounterpartCharCode("A") ➞ 97
+
+            CounterpartCharCode("a") ➞ 65
+
+            The argument will always be a single character.
+            Not all inputs will have a counterpart (e.g. numbers), in which case return the inputs char code.
+
+            */
+
+            //char symbol = 'A';
+
+            //if (Char.IsUpper(symbol))
+            //    symbol= Char.ToLower(symbol);
+            //else if (Char.IsLower(symbol))
+            //    symbol = Char.ToUpper(symbol);
+            //Console.WriteLine((int)symbol);
+
+            //Console.ReadKey();
+
+
+
+            /* Return the Highest and Lowest Numbers! <--- 41th
+            
+            Create a function that accepts a string of space separated numbers and returns the highest and lowest number (as a string).
+
+            HighLow("1 2 3 4 5") ➞ "5 1"
+
+            HighLow("1 2 -3 4 5") ➞ "5 -3"
+
+            HighLow("1 9 3 4 -5") ➞ "9 -5"
+
+            HighLow("13") ➞ "13 13"
+
+            All numbers are valid Int32, no need to validate them.
+            There will always be at least one number in the input string.
+            Output string must be two numbers separated by a single space, and highest number is first.
+
+            */
+
 
 
         }
