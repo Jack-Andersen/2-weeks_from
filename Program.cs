9ad887a17e31c4552cd5a03f78e0d4703bedc6e4
Jack-Andersen <Jack-Andersen@users.noreply.github.com>
2020-05-15 12:16:28 +0200

    Done with 72,73. Working on 74.

--- Program.cs
+++ Program.cs
@@ -2596,6 +2596,78 @@ namespace Edabit__the_2_weeks_we_re_not_in_school
 
             */
 
+            //string[] arr = { "mavis", "senaida", "letty" };
+
+            //int x = 0;
+            //foreach (string s in arr)
+            //{
+            //    string name = string.Empty;
+            //    name += s[0].ToString().ToUpper();
+            //    for (int i = 1; i < s.Length; i++)
+            //    {
+            //        name += s[i].ToString().ToLower();
+            //    }
+            //    arr[x++] = name;
+            //}
+
+            //Console.WriteLine(arr);
+
+            //Console.ReadKey();
+
+
+
+            /* ReverseAndNot! <--- 73th
+            
+            Write a function that takes an integer i and returns a string with the integer backwards followed by the original integer.
+
+            To illustrate:
+
+            123
+
+            We reverse 123 to get 321 and then add 123 to the end, resulting in 321123.
+
+            ReverseAndNot(123) ➞ "321123"
+
+            ReverseAndNot(152) ➞ "251152"
+
+            ReverseAndNot(123456789) ➞ "987654321123456789"
+
+            Bonus: By using System.Linq this should be completed in one line of code.
+
+            */
+
+            //int i = 123;
+
+            //int num = 0, orig = i;
+
+            //while (i != 0)
+            //{
+            //    num = num * 10 + (i % 10);
+            //    i /= 10;
+            //}
+
+            //Console.WriteLine(num.ToString() + orig.ToString());
+
+
+
+            /* Smooth Sentences! <--- 74th
+            
+            Carlos is a huge fan of something he calls smooth sentences. A smooth sentence is one where the last letter of each word is identical to the first letter the following word.
+
+            To illustrate, the following would be a smooth sentence: "Carlos swam masterfully."
+
+            Since "Carlos" ends with an "s" and swam begins with an "s" and swam ends with an "m" and masterfully begins with an "m".
+
+            IsSmooth("Marta appreciated deep perpendicular right trapezoids") ➞ true
+
+            IsSmooth("Someone is outside the doorway") ➞ false
+
+            IsSmooth("She eats super righteously") ➞ true
+
+            N/A
+
+            */
+
 
 
         }
