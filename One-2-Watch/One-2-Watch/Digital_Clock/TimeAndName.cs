e8b6749fc0126cf9cf7bedfb0087425524f888fe
Jack-Andersen <Jack-Andersen@users.noreply.github.com>
2020-06-26 15:20:47 +0200

    One-2-Watch
    Still need some work ^^"

---
+++ One-2-Watch/One-2-Watch/Digital_Clock/TimeAndName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Digital_Clock
+{
+    public class TimeAndName
+    {
+        private string v1;
+        public TimeSpan timeSpan;
+        private string v2;
+
+        public TimeAndName(string v1, TimeSpan timeSpan, string v2)
+        {
+            this.v1 = v1;
+            this.timeSpan = timeSpan;
+            this.v2 = v2;
+        }
+
+        public string TimeString { get; internal set; }
+    }
+}
